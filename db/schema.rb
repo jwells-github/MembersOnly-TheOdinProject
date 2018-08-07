75fcb3eaf3bd64bcd53e98d9485738b51805d6a4
Jake Wells <jakedwells@outlook.com>
2018-08-07 08:25:19 +0000

    add initial users database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_080330) do
+ActiveRecord::Schema.define(version: 2018_08_07_081741) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
