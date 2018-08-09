ca7a9abe007aa8b3c14c2a5ee6723a92bbf1eae7
Jake Wells <jakedwells@outlook.com>
2018-08-09 10:37:29 +0000

    option to remember user after browser closed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     # If the given user was valid, and the given password was correct
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = "logged in"
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
     else
       flash[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url  
   end
 end
